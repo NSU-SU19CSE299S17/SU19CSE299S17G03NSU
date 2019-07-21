9ff73577d84458fefef36ac7e53ddde42799a40c
SarahSuad <sarah.suad@northsouth.edu>
2019-07-21 22:44:02 +0600

    Functionility #14: Retrieving db values and placing them into mylist table
    Description: -Values that were preset into the database were retrieved and placed in the mylist table

--- Project Code/source/pages/listdb.js
+++ Project Code/source/pages/listdb.js
@@ -1,6 +1,25 @@
-db.collection('Library').get().then((snapshot)=> {
+const resourcelist=document.querySelector('#resource-list');
+
+function renderList(doc){
+    let li= document.createElement('li');
+    let name= document.createElement('span');
+    let type= document.createElement('lspan');
+
+    li.setAttribute('data-id',doc.id);
+    name.textContent= doc.data().Name;
+    type.textContent= doc.data().Type;
+
+    li.appendChild(name);
+    li.appendChild(type);
+
+    resourcelist.appendChild(li);
+
+}
+
+
+db.collection('Lists').get().then((snapshot)=> {
 snapshot.docs.forEach(doc => {
-    console.log(doc.data())
+    renderList(doc);
 })
 
 })
