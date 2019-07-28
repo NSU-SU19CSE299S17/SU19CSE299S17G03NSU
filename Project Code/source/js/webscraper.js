fc5e750af849fc78dcb707f50180417048841824
Namerlight <shadabchoudhury@gmail.com>
2019-07-28 21:38:41 +0600

    Feature #18: Initial data fetching script through npm-osmosis library for webscraping.

--- Project Code/source/js/webscraper.js
+++ Project Code/source/js/webscraper.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
     document.write("Placeholder Testing")
-    
+
+    var osmosis = require('osmosis');
+
+    var url = 'https://www.amazon.com/Introduction-Algorithms-3rd-MIT-Press/dp/0262033844/ref=zg_bs_491298_1?_encoding=UTF8&psc=1&refRID=4VBHQN1B11GJWRKSYH7H';
+
+    osmosis
+        .get(url)
+        .set({
+            heading: 'h1'
+        })
+        .data(item => console.log(item));
+
 });
