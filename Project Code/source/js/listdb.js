4d8566c3ae80776d93d16e3d87eba81adc01695e
SarahSuad <sarah.suad@northsouth.edu>
2019-07-28 19:41:51 +0600

    Bug #17: Removing unnecessary lines of code from listdb.js
    Description:-Deleting append function code
    -Renaming function to sortlist

--- Project Code/source/js/listdb.js
+++ Project Code/source/js/listdb.js
@@ -1,19 +1,16 @@
-const resourcelist= document.querySelector('#resource-list');
 
-function renderlist(doc){
+function sortlist(doc){
 
 name.textContent=doc.data().name;
 type.textContent=doc.data().type;
 
-$("#resource-name").append(name);
-$("#resource-type").append(name);
 
 }
 
 db.collection('Lists').get().then((snapshot) => {
  snapshot.docs.forEach(doc => {
    
-    renderlist(doc);
+    sortlist(doc);SS
 
  })
 })
