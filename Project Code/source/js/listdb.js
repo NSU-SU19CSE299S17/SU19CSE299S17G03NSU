d391963e83c5eca3b5f24fc92999cffa82a9cbdf
SarahSuad <sarah.suad@northsouth.edu>
2019-07-28 19:50:34 +0600

    Bug #17: Rewriting code of sortlist function
    Description:-Rewriting code inside sortlist function of listdb.js
    -Values are retrived from db and sort into approriate columns
    -New rows are created for each new document retrived from db
    git commit -a -m Bug

--- Project Code/source/js/listdb.js
+++ Project Code/source/js/listdb.js
@@ -1,16 +1,35 @@
 
 function sortlist(doc){
 
-name.textContent=doc.data().name;
-type.textContent=doc.data().type;
+const rname= doc.data().Name;
+const rtype=doc.data().Type;
+const rurl=doc.data().Url;
+var tableRef = document.getElementById('resource-list');
 
+  // Insert a row in the table after the last row
+  var newRow   = tableRef.insertRow(-1);
+
+  // Insert a cell in the row, starting from index 0
+  var newCell  = newRow.insertCell(0);
+  var newCell1  = newRow.insertCell(1);
+  var newCell2  = newRow.insertCell(2);
+
+
+  // Append a text node to the cell
+  var newText  = document.createTextNode(rname);
+  var newText1  = document.createTextNode(rurl);
+  var newText2  = document.createTextNode(rtype);
+
+  newCell.appendChild(newText);
+  newCell1.appendChild(newText1);
+  newCell2.appendChild(newText2);
 
 }
 
 db.collection('Lists').get().then((snapshot) => {
  snapshot.docs.forEach(doc => {
    
-    sortlist(doc);SS
+    sortlist(doc);
 
  })
 })
