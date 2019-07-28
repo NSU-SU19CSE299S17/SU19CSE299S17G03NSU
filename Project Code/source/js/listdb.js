fde1fa367d5c06aa848f08cb7290181997a2f241
SarahSuad <sarah.suad@northsouth.edu>
2019-07-28 19:14:33 +0600

    Bug #17: Removed unnecessary line of code from listdb.js
    Description: -Removed commented out lines of code that were in listdb.js
    -Moved listdb.js from 'pages' folder to 'js' folder
    -Updated location of listdb.js in script tags of mylist.html
    -Deleted unnecessary copy of file listdb.js in 'pages' folder

--- Project Code/source/js/listdb.js
+++ Project Code/source/js/listdb.js
@@ -1,6 +1,19 @@
-db.collection('Library').get().then((snapshot)=> {
-snapshot.docs.forEach(doc => {
-    console.log(doc.data())
-})
+const resourcelist= document.querySelector('#resource-list');
 
+function renderlist(doc){
+
+name.textContent=doc.data().name;
+type.textContent=doc.data().type;
+
+$("#resource-name").append(name);
+$("#resource-type").append(name);
+
+}
+
+db.collection('Lists').get().then((snapshot) => {
+ snapshot.docs.forEach(doc => {
+   
+    renderlist(doc);
+
+ })
 })
