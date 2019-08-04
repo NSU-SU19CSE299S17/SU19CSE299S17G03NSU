61354d1543a4fbe320a760d550bff981b04c650d
Namerlight <shadabchoudhury@gmail.com>
2019-08-04 21:19:59 +0600

    Feature #18: Added basic code for fetching the title of a youtube video.

--- Project Code/source/js/mylist.js
+++ Project Code/source/js/mylist.js
@@ -1,13 +1,29 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
+let url = 'https://www.coursera.org/learn/algorithms-part1'
+
 request({
     method: 'GET',
-    url: 'https://www.youtube.com/watch?v=Dk0axPbD2pc'
+    url: url
 
 }, (err, res, html) => {
 
     if (err) return console.error(err);
+
+    let $ = cheerio.load(html);
+    let title = $('title').text();
+
+    if (url.search('coursera')) {
+
+        var pos = title.indexOf("Coursera");
+        var trimmed_title = title.slice(0, pos+8);
+        console.log(trimmed_title);
+
+    } else {
+
+        console.log(title);
+    }
 
 });
 
