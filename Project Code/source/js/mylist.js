0c71d86e3ec681719d16d23a2f8d883887f955cd
Namerlight <shadabchoudhury@gmail.com>
2019-08-04 20:54:48 +0600

    Installed request and cheerio packages and added basic webscraping framework to fetch the html code of a webpage

--- Project Code/source/js/mylist.js
+++ Project Code/source/js/mylist.js
@@ -1,6 +1,21 @@
+const request = require('request');
+const cheerio = require('cheerio');
 
-$(document).ready(function() {
+request({
+    method: 'GET',
+    url: 'https://www.youtube.com/watch?v=Dk0axPbD2pc'
+
+}, (err, res, html) => {
+
+    if (err) return console.error(err);
+
+});
+
+
+/*$(document).ready(function() {
 
     $('#modal1').modal();
-    
-  });
+
+
+
+});*/
