4f090a98fc3c6085449f5b5bd1c91ae6536165c8
SarahSuad <sarah.suad@northsouth.edu>
2019-08-16 02:34:24 +0600

    Bug #10: Moving js code from index.html to index.js page.
    Description: -Js code on index.html was moved to index.js
    -Js code in index.js file was modified so that on pressing 'Reg' or 'Login' button on index page, popup modals would work
    -Modal and modal-content class was defined on index.html

--- Project Code/source/js/index.js
+++ Project Code/source/js/index.js
@@ -1,11 +1,28 @@
 
 
-document.getElementById("about-button").addEventListener("click", scrolltoAbout)
+var regBtn= document.getElementById('regBtn');
+var loginBtn= document.getElementById('loginBtn');
+
+$(document).ready(function() {
+    $('#regBtn').click(function() {
+      console.log("Reg pressed");
+      document.getElementById('register-form').style.display='block';
+    });
+  });
+
+  $(document).ready(function() {
+    $('#loginBtn').click(function() {
+      console.log("Login pressed");
+      document.getElementById('login-form').style.display='block';
+    });
+  });
+
+/*document.getElementById("about-button").addEventListener("click", scrolltoAbout)
 
 function scrolltoAbout() {
     document.getElementById("about-trism").scrollTo();
     document.get('about-trism').scrollIntoView({
         behavior: 'smooth'
     });
-}
+}*/
 
