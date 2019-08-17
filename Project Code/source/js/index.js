c051d6120fcdc4074aad563870e476a7dc48dac7
SarahSuad <sarah.suad@northsouth.edu>
2019-08-17 22:17:43 +0600

    Bug #10: Adding close button to modals on 'index.html' page
    Description: -Js code for close button of Login and Registration modals added
    -'closeBtn' class defined on index.html
    -pseudoclasses ':hover' and ':focus' defined for closeBtn on index.html
    -'closeBtn' class added to modals on index.html

--- Project Code/source/js/index.js
+++ Project Code/source/js/index.js
@@ -2,20 +2,32 @@
 
 var regBtn= document.getElementById('regBtn');
 var loginBtn= document.getElementById('loginBtn');
+var closeBtnReg= document.getElementsByClassName('closeBtn')[0];
+var closeBtnLogin= document.getElementsByClassName('closeBtn')[1];
 
 $(document).ready(function() {
     $('#regBtn').click(function() {
       console.log("Reg pressed");
       document.getElementById('register-form').style.display='block';
     });
-  });
 
-  $(document).ready(function() {
     $('#loginBtn').click(function() {
       console.log("Login pressed");
       document.getElementById('login-form').style.display='block';
     });
+
+    closeBtnReg.onclick = function() {
+      console.log("Closed Reg Modal");
+      document.getElementById('register-form').style.display = "none";
+   }
+
+   closeBtnLogin.onclick = function() {
+    console.log("Closed Login Modal");
+    document.getElementById('login-form').style.display = "none";
+   }
+   
   });
+
 
 /*document.getElementById("about-button").addEventListener("click", scrolltoAbout)
 
