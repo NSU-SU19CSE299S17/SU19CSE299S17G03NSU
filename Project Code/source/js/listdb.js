1e66fff713615ab07950cfb5740cff85da826acb
Namerlight <shadab.choudhury@northsouth.edu>
2019-08-22 13:17:54 +0600

    Feature #26: Added function to detect when an user is logged in and return the user credentials plus user email in the index page.

--- Project Code/source/js/listdb.js
+++ Project Code/source/js/listdb.js
@@ -1,25 +1,25 @@
-function sortlist(doc){
+function sortlist(doc) {
 
-const rname= doc.data().NameOfResource;
-const rtype= doc.data().Type;
-const rurl= doc.data().Url;
+    const rname = doc.data().NameOfResource;
+    const rtype = doc.data().Type;
+    const rurl = doc.data().Url;
 
 
-var tableRef = document.getElementById('resource-list');
+    var tableRef = document.getElementById('resource-list');
 
     // Insert a row in the table after the last row
-    var newRow   = tableRef.insertRow(-1);
+    var newRow = tableRef.insertRow(-1);
 
     // Insert a cell in the row, starting from index 0
-    var newCell  = newRow.insertCell(0);
-    var newCell1  = newRow.insertCell(1);
-    var newCell2  = newRow.insertCell(2);
+    var newCell = newRow.insertCell(0);
+    var newCell1 = newRow.insertCell(1);
+    var newCell2 = newRow.insertCell(2);
 
 
     // Append a text node to the cell
-    var newText  = document.createTextNode(rname);
-    var newText1  = document.createTextNode(rurl);
-    var newText2  = document.createTextNode(rtype);
+    var newText = document.createTextNode(rname);
+    var newText1 = document.createTextNode(rurl);
+    var newText2 = document.createTextNode(rtype);
 
     newCell.appendChild(newText);
     newCell1.appendChild(newText1);
@@ -32,7 +32,7 @@ db.collection('Lists').get().then((snapshot) => {
     console.log(snapshot.docs);
     snapshot.docs.forEach(doc => {
         console.log(doc.id);
-        const id= doc.id;
+        const id = doc.id;
 
         db.collection('Lists').doc(doc.id).collection('Entries').get().then((snapshot) => {
             snapshot.docs.forEach(doc => {
@@ -57,12 +57,13 @@ addItemRef.addEventListener('submit', (event) => {
         })
     });
 
-  
-db.collectionGroup('Entries').where('NameOfList', '==','List1' ).where('Owner','==','User1').get().then(function(querySnapshot) {
 
-  querySnapshot.forEach(function(doc) {
-    console.log(doc.id, ' => ', doc.data());
-    sortlist(doc);
-  });
+    db.collectionGroup('Entries').where('NameOfList', '==', 'List1').where('Owner', '==', 'User1').get().then(function (querySnapshot) {
 
+        querySnapshot.forEach(function (doc) {
+            console.log(doc.id, ' => ', doc.data());
+            sortlist(doc);
+        });
+
+    })
 });
