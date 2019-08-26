7f2a037b87e1ef19545b9fcd8e03627a6500d5da
Namerlight <shadabchoudhury@gmail.com>
2019-08-27 01:10:31 +0600

    Feature #15: Fixed list displaying from the library in mylist page.

--- Project Code/source/js/listdb.js
+++ Project Code/source/js/listdb.js
@@ -44,29 +44,41 @@ firebase.auth().onAuthStateChanged(function (user) {
                 NameOfResource: addItemRef.Rname.value,
                 Type: addItemRef.Rtype.value,
                 URL: addItemRef.Rlink.value,
-                NameOfList: trimListName,
+                NameOfList: listname,
                 Owner: email,
                 Review: addItemRef.Review.value,
                 Tags: addItemRef.Tags.value
-            })
+            });
             console.log("New Item Added to Library.");
 
         });
 
-        db.collectionGroup('Entries').where('NameOfList', '==', trimListName).where('Owner', '==', email).get().then(function (querySnapshot) {
+        /*db.collectionGroup('Entries').where('NameOfList', '==', trimListName).where('Owner', '==', email).get().then(function (querySnapshot) {
 
             querySnapshot.forEach(function (doc) {
                 console.log(doc.id, ' => ', doc.data());
                 sortlist(doc);
             });
 
+        });*/
+
+        db.collection('Library').where('NameOfList','==', listname).get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+
+                const id = doc.id;
+                console.log(doc.id);
+                sortlist(doc);
+
+            });
         });
 
         function sortlist(doc) {
 
             const rname = doc.data().NameOfResource;
             const rtype = doc.data().Type;
-            const rurl = doc.data().Url;
+            const rurl = doc.data().URL;
+            const rreview = doc.data().Review;
+            const rtags = doc.data().Tags
             var tableRef = document.getElementById('resource-list');
 
             // Insert a row in the table after the last row
@@ -76,16 +88,24 @@ firebase.auth().onAuthStateChanged(function (user) {
             var newCell = newRow.insertCell(0);
             var newCell1 = newRow.insertCell(1);
             var newCell2 = newRow.insertCell(2);
+            var newCell3 = newRow.insertCell(3);
+            var newCell4 = newRow.insertCell(4);
+
 
 
             // Append a text node to the cell
             var newText = document.createTextNode(rname);
             var newText1 = document.createTextNode(rurl);
             var newText2 = document.createTextNode(rtype);
+            var newText3 = document.createTextNode(rreview);
+            var newText4 = document.createTextNode(rtags);
+
 
             newCell.appendChild(newText);
             newCell1.appendChild(newText1);
             newCell2.appendChild(newText2);
+            newCell3.appendChild(newText3);
+            newCell4.appendChild(newText4);
 
         }
     } else {
